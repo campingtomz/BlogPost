164683335859118ed7ba86a5d7040e53bb2f13c2
Thomas Zanis <56691946+campingtomz@users.noreply.github.com>
2020-07-30 15:54:50 -0400

    update
    added email service and updated git ignore

--- App_Start/IdentityConfig.cs
+++ App_Start/IdentityConfig.cs
@@ -11,6 +11,9 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using blog.Models;
+using System.Net.Mail;
+using System.Web.Configuration;
+using System.Net;
 
 namespace blog
 {
@@ -21,9 +24,36 @@ namespace blog
             // Plug in your email service here to send an email.
             return Task.FromResult(0);
         }
+        public async Task SendAsync(MailMessage message)
+        {
+            var GmailUsername = WebConfigurationManager.AppSettings["username"];
+            var GmailPassword = WebConfigurationManager.AppSettings["password"];
+            var host = WebConfigurationManager.AppSettings["host"];
+            int port = Convert.ToInt32(WebConfigurationManager.AppSettings["port"]);
+            using (var smtp = new SmtpClient()
+            {
+                Host = host,
+                Port = port,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(GmailUsername, GmailPassword)
+            })
+            {
+                try
+                {
+                    await smtp.SendMailAsync(message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+        }
     }
 
-    public class SmsService : IIdentityMessageService
+        public class SmsService : IIdentityMessageService
     {
         public Task SendAsync(IdentityMessage message)
         {
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using blog.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Configuration;
+using System.Threading.Tasks;
 
 namespace blog.Controllers
 {
@@ -27,8 +31,39 @@ namespace blog.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+            EmailModel model = new EmailModel();
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public async Task<ActionResult> Contact(EmailModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var body = "<p>Email From: <bold>{0}</bold>({1})</p><p>Message: </p><p>{2}</p>";
+                    var from = "MyPortfolio<example@email.com>";
+                    model.Body = "This is a message from your Blog site. The name and the email of the contact person ";
+                    var email = new MailMessage(from, ConfigurationManager.AppSettings["emailto"])
+                    {
+                        Subject = "Blog Contact Message",
+                        Body = string.Format(body, model.FromName, model.FromEmail, model.Body),
+                        IsBodyHtml = true
+                    };
+                    var svc = new EmailService();
+                    await svc.SendAsync(email);
 
-            return View();
+                    return View(new EmailModel());
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    await Task.FromResult(0);
+                }
+            }
+            return View(model);
         }
     }
 }
